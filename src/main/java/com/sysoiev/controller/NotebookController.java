68defcddddcd1379f84ccec7f3ecb162c38c13df
Bada1208 <sysoievbogdan@gmail.com>
2020-04-09 09:54:22 +0200

    Initial commit - first working pattern

---
+++ src/main/java/com/sysoiev/controller/NotebookController.java
@@ -0,0 +1,24 @@
+package com.sysoiev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class NotebookController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allNotes() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("notes");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public ModelAndView editPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("editPage");
+        return modelAndView;
+    }
+}
